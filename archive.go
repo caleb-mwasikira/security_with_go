d381b530ca7efc14fa97a32a952275132cb194b4
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-04-24 18:17:16 +0300

    implemented ZIP archive extraction

--- archive.go
+++ archive.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"archive/zip"
+	"crypto/md5"
+	"crypto/rand"
 	"fmt"
+	"io"
 	"io/fs"
 	"log"
 	"os"
@@ -15,6 +18,10 @@ func ZipArchive(source string, dest string) error {
 	stat, err := os.Stat(source)
 	if err != nil {
 		return err
+	}
+
+	if !stat.IsDir() {
+		return fmt.Errorf("%s is not a directory", source)
 	}
 
 	// check if dest is a valid directory
@@ -90,13 +97,7 @@ func ZipArchive(source string, dest string) error {
 		if err != nil {
 			return err
 		}
-
-		// close in_file
-		defer func() {
-			if err := in_file.Close(); err != nil {
-				log.Panic(err)
-			}
-		}()
+		defer in_file.Close()
 
 		err = CopyFile(in_file, out_file)
 		if err != nil {
@@ -114,6 +115,105 @@ func ZipArchive(source string, dest string) error {
 	err = zip_writer.Close()
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func genNewFilename() (string, error) {
+	data := make([]byte, 32)
+	_, err := rand.Read(data)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate new filename")
+	}
+
+	new_filename := string(md5.New().Sum(data))
+	return new_filename, nil
+}
+
+func deleteFileIfExists(filepath string) bool {
+	stat, _ := os.Stat(filepath)
+	if stat != nil {
+		err := os.Remove(filepath)
+		return err == nil
+	}
+
+	return true
+}
+
+func Unzip(source string) error {
+	// open the archive with with a zip reader
+	zip_reader, err := zip.OpenReader(source)
+	if err != nil {
+		return err
+	}
+	defer zip_reader.Close()
+
+	// define the dest root directory
+	parent := filepath.Dir(source)
+	dirname := filepath.Base(source)
+
+	if filepath.Ext(dirname) != ".zip" {
+		return fmt.Errorf("expected archive in ZIP format")
+	}
+
+	// strip .zip suffix from dirname to get new dirname
+	new_dirname, found := strings.CutSuffix(dirname, ".zip")
+	if !found {
+		return fmt.Errorf("extension .zip not found in archive filename")
+	}
+
+	if len(new_dirname) == 0 {
+		new_dirname, err = genNewFilename()
+		if err != nil {
+			return fmt.Errorf("failed to generate new dirname for archive")
+		}
+	}
+	dest := filepath.Join(parent, new_dirname)
+
+	// delete destination dir if already exists
+	success := deleteFileIfExists(dest)
+	if !success {
+		return fmt.Errorf("cannot extract archive on existing directory %v", dest)
+	}
+
+	// iterate through each of the files found in the archive
+	for _, file := range zip_reader.Reader.File {
+		// open the file like a normal file
+		zipped_file, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer zipped_file.Close()
+
+		// specify what the extracted filename should be
+		extracted_filepath := filepath.Join(dest, file.Name)
+
+		if file.FileInfo().IsDir() {
+			log.Printf("creating directory %v ...\n", extracted_filepath)
+			err := os.MkdirAll(extracted_filepath, 0700)
+			if err != nil {
+				return fmt.Errorf("failed to create directory %v: %v", extracted_filepath, err)
+			}
+		} else {
+			log.Printf("extracting file %v ...\n", file.Name)
+
+			out_file, err := os.OpenFile(
+				extracted_filepath,
+				os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
+				0700,
+			)
+			if err != nil {
+				return err
+			}
+			defer out_file.Close()
+
+			// copy contents of archive file into out_file
+			_, err = io.Copy(out_file, zipped_file)
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 
 func main() {
 	src := "/home/netrunner/College"
+	archive := "/home/netrunner/College.zip"
 
 	err := ZipArchive(src, "")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	log.Println("done archiving files")
+
+	err = Unzip(archive)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("done extracting zip file")
 }
