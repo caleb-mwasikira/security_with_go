90dc31ca6be9320498de8bc46608c2d469f16e68
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-05-29 14:20:04 +0300

    listing all network interfaces on machine

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/caleb-mwasikira/security_with_go
 
 go 1.22.0
+
+require github.com/google/gopacket v1.1.19 // indirect
--- main.go
+++ main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
-	"github.com/caleb-mwasikira/security_with_go/codecs"
+	"github.com/google/gopacket/pcap"
 )
 
 func main() {
-	source := "/home/netrunner/College"
-	dest := "/home/netrunner/College.zip"
-
-	// dest, err := arch.ZipArchive(source, "")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// log.Printf("done archiving source %v into destination %v", source, dest)
-
-	// archive := "~/Videos.zip"
-	// err = arch.Unzip(archive)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	err := codecs.GzipCompress(dest, codecs.PRESERVE_DIR)
+	// list all interfaces on current machine
+	devices, err := pcap.FindAllDevs()
 	if err != nil {
-		log.Fatalf("GzipCompress failed to compress file %v - error: %v", source, err)
+		log.Fatalf("failed to list network interfaces: %v", err)
+	}
+
+	for _, device := range devices {
+		fmt.Println("Device Name: ", device.Name)
+		fmt.Println("Device Description: ", device.Description)
+		if len(device.Addresses) == 0 {
+			println()
+		}
+
+		for _, address := range device.Addresses {
+			fmt.Println("- IP address: ", address.IP)
+			fmt.Println("- Subnet Mask: ", address.Netmask)
+			println()
+		}
 	}
 }
