02c43f83288c81d76f92d4b27bfe3fd29f26a803
Caleb Mwasikira <calebmwasikira@gmail.com>
2024-05-31 08:40:41 +0300

    user authentication system thats stores hashed passwords onto a custom shadow file

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.0
 
 require (
 	github.com/google/gopacket v1.1.19 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
 )
--- main.go
+++ main.go
@@ -1,89 +1,181 @@
 package main
 
 import (
+	"bufio"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"path"
+	"slices"
 	"strings"
-	"time"
 
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pcap"
-	"github.com/google/gopacket/pcapgo"
+	"github.com/joho/godotenv"
 )
 
 var (
-	device      = "lo"
-	snapshotLen = 1024
-	promiscuous = false
-	err         error
-	timeout     = 30 * time.Second
-	handle      *pcap.Handle
-	packetCount = 0
+	secret_key    string
+	password_file string = "test_data/shadow"
 )
 
-// list all interfaces on current machine
-func listNetworkDevice(name string) {
-	devices, err := pcap.FindAllDevs()
+func generateSalt() (string, error) {
+	random_bytes := make([]byte, 32)
+	_, err := rand.Read(random_bytes)
 	if err != nil {
-		log.Fatalf("failed to list network interfaces: %v", err)
+		return "", err
 	}
+	salt := hex.EncodeToString(random_bytes)
+	return salt, nil
+}
 
-	for _, device := range devices {
-		if device.Name == name {
-			fmt.Printf("%v [%v] (%v)\n", device.Name, device.Flags, device.Description)
+func hashPassword(plain_text, salt string) string {
+	hash := hmac.New(sha256.New, []byte(secret_key))
+	io.WriteString(hash, plain_text+salt)
+	hashed_value := hash.Sum(nil)
+	return hex.EncodeToString(hashed_value)
+}
 
-			for _, address := range device.Addresses {
-				fmt.Println("- IP address: ", address.IP)
-				fmt.Println("- Subnet Mask: ", address.Netmask)
-			}
+func inputStr(input_type, prompt string, min_len int) string {
+	var value string
 
-			println()
+	for {
+		fmt.Print(prompt)
+		fmt.Scanf("%s", &value)
+
+		if len(value) > min_len {
+			break
+		}
+		fmt.Printf("%v cannot be less than %v characters: please try again\n\n", input_type, min_len)
+	}
+	return value
+}
+
+/*
+asks the user to enter their username and password
+*/
+func getLoginCredentials() (string, string) {
+	min_uname_len := 3
+	min_pass_len := 8
+	username := inputStr("username", "Enter your username: ", min_uname_len)
+	password := inputStr("password", "Enter your password: ", min_pass_len)
+
+	return username, password
+}
+
+/*
+saves user login credentials to password file in the
+format:
+
+	username:$id$salt$hashed
+*/
+func savePassword(username, salt, hashed_password string) error {
+	file, err := os.OpenFile(password_file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// $5$ is sha-256 hashing algorithm
+	// https://www.cyberciti.biz/faq/understanding-etcshadow-file/
+	hashing_alg := 5
+	line := fmt.Sprintf("%v:$%v$%v$%v\n", username, hashing_alg, salt, hashed_password)
+	_, err = file.WriteString(line)
+	return err
+}
+
+/*
+search for username in password file
+*/
+func findUserAccount(username string) (string, error) {
+	file, err := os.OpenFile(password_file, os.O_RDONLY, 0666)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	// read file line-by-line until we find the user or not
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, username) {
+			return line, nil
 		}
 	}
+	return "", fmt.Errorf("user account with username '%v' not found", username)
 }
 
 func main() {
-	listNetworkDevice(device)
-
-	// open device in live capture mode
-	handle, err = pcap.OpenLive(device, int32(snapshotLen), promiscuous, timeout)
+	// load secret key from env file
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("failed to open network interface %v; %v", device, err)
+		log.Fatalf("error loading .env file; %v", err)
 	}
-	defer handle.Close()
-	fmt.Printf("sniffing interface %v...\n", device)
+	secret_key = os.Getenv("SECRET_KEY")
 
-	// set filter
-	filter := "tcp and port 8080"
-	err = handle.SetBPFFilter(filter)
-	if err != nil {
-		log.Fatalf("failed to set BPF filter on captured packets; %v", err)
-	}
+	// select user action
+	user_action := 0
+	valid_user_actions := []int{1, 2, 3}
 
-	// create new output pcap file and write header
-	filename := strings.ReplaceAll(filter, " ", "_")
-	filename = path.Join("build", fmt.Sprintf("%v.pcap", filename))
-	fmt.Printf("creating pcap file %v...\n", filename)
+	for {
+		fmt.Println("Select action:\n   1. SignUp\n   2. Login\n   3. Exit")
+		fmt.Print("> ")
+		fmt.Scanf("%d", &user_action)
 
-	file, err := os.Create(filename)
-	if err != nil {
-		log.Fatalf("failed to create output pcap file; %v", err)
-	}
-	pcap_writer := pcapgo.NewWriter(file)
-	pcap_writer.WriteFileHeader(uint32(snapshotLen), layers.LinkTypeEthernet)
-	defer file.Close()
-
-	packetSrc := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range packetSrc.Packets() {
-		pcap_writer.WritePacket(packet.Metadata().CaptureInfo, packet.Data())
-
-		// only capture 50 packets and the stop
-		if packetCount > 50 {
+		if slices.Contains(valid_user_actions, user_action) {
 			break
 		}
-		packetCount++
+		fmt.Printf("incorrect choice %v\n", user_action)
+	}
+	println()
+
+	switch user_action {
+	case 1: // signup
+		fmt.Println("creating a new account")
+		username, password := getLoginCredentials()
+
+		// first check if username already exists
+		user_account, _ := findUserAccount(username)
+		if len(user_account) > 0 {
+			log.Fatalf("account with username %v already exists", username)
+		}
+
+		salt, err := generateSalt()
+		if err != nil {
+			log.Fatalf("error generating salt; %v", err)
+		}
+
+		hashed_password := hashPassword(password, salt)
+		err = savePassword(username, salt, hashed_password)
+		if err != nil {
+			log.Fatalf("error saving password to file; %v", err)
+		}
+		fmt.Println("account created successfuly")
+
+	case 2: // login
+		fmt.Println("logging in to your existing account")
+		username, password := getLoginCredentials()
+		user_account, err := findUserAccount(username)
+		if err != nil {
+			log.Fatalf("error logging in: %v", err)
+		}
+		id_salt_hash := strings.TrimPrefix(user_account, fmt.Sprintf("%v:", username))
+		id_salt_hash = strings.TrimPrefix(id_salt_hash, "$")
+		fields := strings.Split(id_salt_hash, "$")
+		salt := fields[1]
+		db_password_hash := fields[2]
+
+		// verify password match
+		hashed_password := hashPassword(password, salt)
+		if hashed_password == db_password_hash {
+			fmt.Println("Login success")
+		} else {
+			fmt.Println("Login failed")
+		}
+	case 3:
+		fmt.Println("exiting program... Bye :)")
+		os.Exit(1)
 	}
 }
